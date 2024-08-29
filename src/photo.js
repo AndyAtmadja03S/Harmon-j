079507b57c62127d6f62b6d74b32da302fff1ba3
Andy Atmadja <andyatmadja@192-168-1-107.tpgi.com.au>
2024-08-29 16:29:22 +1000

    Rotating images

---
+++ src/photo.js
@@ -0,0 +1,24 @@
+const photoBox = document.querySelector('.photo-box');
+const images = photoBox.querySelectorAll('img');
+
+let currentImageIndex = 0;
+let interval;
+
+function nextImage() {
+    images.forEach((img, index) => {
+        img.style.opacity = index === currentImageIndex ? '1' : '0';
+    });
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+photoBox.addEventListener('mouseover', () => {
+    nextImage();
+    interval = setInterval(nextImage, 2000); 
+});
+
+photoBox.addEventListener('mouseout', () => {
+    clearInterval(interval);
+    images.forEach(img => img.style.opacity = '0'); 
+    images[0].style.opacity = '1'; 
+    currentImageIndex = 0;
+});
